3fde47c46b52ebd00b7d2d87d76da94238240934
ninh137 <adzekk01@gmail.com>
2021-04-14 19:22:07 +0700

    Initial Project

---
+++ HeightCounter/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HighCount {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        bool IsValid() {
+            if (textBox1.Text.Trim().Length == 0) {
+                MessageBox.Show("bạn không thể đéo nhập gì cả nhé.");
+                return false;
+            }
+            if (!Regex.IsMatch(textBox1.Text.Trim(), @"^[0-9]+$")) {
+                MessageBox.Show("bạn phải nhập SỐ chứ???.");
+                return false;
+            }
+            int height = int.Parse(textBox1.Text);
+            if (height < 50) {
+                MessageBox.Show("làm đéo gì thấp thế dude??? lừa vl.");
+                return false;
+            } else if (height > 240) {
+                MessageBox.Show("bạn cao thế :), đi lấy kỷ lục guiness đi.");
+                return false;
+            }
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            if (!IsValid()) {
+                return;
+            }
+
+            int height = int.Parse(textBox1.Text);
+            if(textBox1.Text.Length == 3) {
+                string m = textBox1.Text.Substring(0, 1);
+                string cm = textBox1.Text.Substring(1);
+                MessageBox.Show("Chiều cao của bạn là " + m + "m" + cm);
+            } else if(textBox1.Text.Length == 2) {
+                MessageBox.Show("Chiều cao của bạn là " + height + "cm" );
+            }
+
+            if (height < 150) {
+                MessageBox.Show("Bạn thấp vl :)");
+            } 
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                button1_Click(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ HeightCounter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Phần mềm tính chiều cao uy tín")]
+[assembly: AssemblyDescription("Phần mềm tính chiều cao có sử dụng AI (chạy bằng điện) để tính toán")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FPT University")]
+[assembly: AssemblyProduct("Phần mềm tính chiều cao uy tín")]
+[assembly: AssemblyCopyright("Copyright © FPT University 2021")]
+[assembly: AssemblyTrademark("D101")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1f15cd9a-37a7-4118-989b-2dab0bde6c3e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
