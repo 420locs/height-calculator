b7029fa124f7cdf4705f89449974cfbfe92ceb94
ninh137 <adzekk01@gmail.com>
2021-04-14 19:47:17 +0700

    Create Validator library

---
+++ ValidateDataLibrary/Validator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ValidateDataLibrary {
+    public static class Validator {
+        public static bool IsEmpty(string txt) {
+            return txt.Trim().Length == 0;
+        }
+        public static bool IsNumberic(string txt) {
+            return Regex.IsMatch(txt.Trim(), @"^[0-9]+$");
+        }
+    }
+}
